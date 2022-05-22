948831ff19b4a43c2c0046c36cf049937444e460
Purgeme <parvesh@curioventures.com>
2022-05-22 17:11:42 +0530

    Working .so example, change in path of tflites

---
+++ mediapipe/examples/desktop/sample_test/main.cc
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "hello_world.h"
+
+void hello(){
+    printf("Hello World");
+}
+
+void holistic_tracking(){
+    test();
+}
---
+++ mediapipe/examples/desktop/sample_test/test.cc
@@ -0,0 +1,6 @@
+#include "hello_world.h"
+
+int main(){
+    hello();
+    return 0;
+}
